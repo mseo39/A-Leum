83373bc4507c44ee9b2eb96295e83ece53058f14
KimTaewan <wwan13@naver.com>
2020-10-31 19:05:35 +0900

    [taewan] User모델 생성

--- user/models.py
+++ user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    """ 커스텀 유저 모델 """
+
+    name = models.CharField(max_length = 50)
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_CHOICES = (
+        (GENDER_MALE,"male"),
+        (GENDER_FEMALE,"female")
+    )
+    gender = models.CharField(max_length = 10,choices=GENDER_CHOICES)
+    age = models.IntegerField(default=0)
+    phone = models.CharField(max_length = 30)
+
